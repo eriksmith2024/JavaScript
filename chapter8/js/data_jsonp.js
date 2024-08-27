a801c9ed1ac9ed9c4bf397991440a733456a9675
eriksmith2024 <eriksmith.career@hotmail.com>
2024-08-27 20:53:04 +0100

    update

--- chapter8/js/data_jsonp.js
+++ chapter8/js/data_jsonp.js
@@ -1,35 +1,15 @@
-// NOTE: If you run this file locally
-// You will not get a server status
-// You can comment out lines 9 and 26 to make it work locally
+function showEvents(data) {     //Callback when JSON laods
+  var NewContent = '';          //Variable to hold HTML
 
-var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
+// BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
+for (var i = 0; i < data.events.length; i++) { // loop through data
+  newContent += '<div class="event">';
+  newContent += '<img src="' + data.events[i].map + '"';
+  newContent += 'alt="' + data.events[i].location + '" />';
+  newContent += '<p><b>' + data.events[i].location + '</b><br>';
+  newContent += data.events[i].date + '</p>';
+  newContent +='</div>';
+}
 
-xhr.onload = function() {                       // When readystate changes
-  // The following conditional check will not work locally - only on a server
-  //if(xhr.status === 200) {                      // If server status was ok
-    responseObject = JSON.parse(xhr.responseText);
-
-    // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
-    var newContent = '';
-    for (var i = 0; i < responseObject.events.length; i++) { // Loop through object
-      newContent += '<div class="event">';
-      newContent += '<img src="' + responseObject.events[i].map + '" ';
-      newContent += 'alt="' + responseObject.events[i].location + '" />';
-      newContent += '<p><b>' + responseObject.events[i].location + '</b><br>';
-      newContent += responseObject.events[i].date + '</p>';
-      newContent += '</div>';
-    }
-
-    // Update the page with the new content
-    document.getElementById('content').innerHTML = newContent;
-
-  //}
-};
-
-xhr.open('GET', 'data/data.json', true);        // Prepare the request
-xhr.send(null);                                 // Send the request
-
-// When working locally in Firefox, you may see an error saying that the JSON is not well-formed.
-// This is because Firefox is not reading the correct MIME type (and it can safely be ignored).
-
-// If you get it on a server, you may need to se the MIME type for JSON on the server (application/JSON).
+// update the page with the new content
+}
