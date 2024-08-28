895471533abf2479719fde5a2e4adb4fb0f12425
eriksmith2024 <eriksmith.career@hotmail.com>
2024-08-28 12:53:05 +0100

    bug fix

--- chapter8/js/data_jsonp.js
+++ chapter8/js/data_jsonp.js
@@ -2,12 +2,12 @@ function showEvents(data) {                           // Callback when JSON load
   var newContent = '';                                // Variable to hold HTML
  
     // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
-    for (var i = 0; i < data.showEvents.length; i++) {   // Loop through object
-      newContent += '<div class="showevent">';
-      newContent += '<img src="' + data.showEventsevents[i].map + '" ';
-      newContent += 'alt="' + data.showEventsevents[i].location + '" />';
-      newContent += '<p><b>' + data.showEventsevents[i].location + '</b><br>';
-      newContent += data.showEventsevents[i].date + '</p>';
+    for (var i = 0; i < data.events.length; i++) {    // Loop through object
+      newContent += '<div class="event">';
+      newContent += '<img src="' + data.events[i].map + '" ';
+      newContent += 'alt="' + data.events[i].location + '" />';
+      newContent += '<p><b>' + data.events[i].location + '</b><br>';
+      newContent += data.events[i].date + '</p>';
       newContent += '</div>';
     }
 
