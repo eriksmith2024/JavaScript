7f4699907ecd121ba6e58fe7a65f13b3ae7e892a
eriksmith2024 <eriksmith.career@hotmail.com>
2024-08-28 13:38:52 +0100

    ajax bug fix

--- chapter8/js/jq_load.js
+++ chapter8/js/jq_load.js
@@ -1,16 +1,11 @@
-function showEvents(data) {                           // Callback when JSON loads
-  var newContent = '';                                // Variable to hold HTML
- 
-    // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
-    for (var i = 0; i < data.events.length; i++) {    // Loop through object
-      newContent += '<div class="event">';
-      newContent += '<img src="' + data.events[i].map + '" ';
-      newContent += 'alt="' + data.events[i].location + '" />';
-      newContent += '<p><b>' + data.events[i].location + '</b><br>';
-      newContent += data.events[i].date + '</p>';
-      newContent += '</div>';
-    }
+$('nav a').on('click', function(e) {                                   //user clicks nav link
+  e.preventDefault();                                                  //stop loading new link
+  var url = this.href;                                                  // get value of href
 
-    // Update the page with the new content
-    document.getElementById('content').innerHTML = newContent;
-}
+  $('nav .current').removeClass('current');                            //clear current indicator
+  $(this).addClass('current');                                       // new current indicator
+
+  $('#container').remove();                                          //remove old content
+  $('#content').load(url + ' #content').hide().fadeIn('slow');       // new content
+
+});
