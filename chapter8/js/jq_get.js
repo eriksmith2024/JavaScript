19d2eb61a2fe1726d11613e6c85a6c486696c8ba
eriksmith2024 <eriksmith.career@hotmail.com>
2024-08-29 10:50:21 +0100

    my ammendments to code

--- chapter8/js/jq_get.js
+++ chapter8/js/jq_get.js
@@ -1,14 +1,28 @@
-// NOTE: This example will only work if you run it on a web server (it will not work locally)
+$(document).ready(function() {
+  console.log('jQuery loaded');  // This should print when the DOM is ready
 
-// This first variable creates the t-shirt options, the HTML for it is shown on p395
-var vote = '<div id="vote"><div class="third"><a href="http://example.org?tshirt=gray"><img src="img/t-gray.png" id="gray" alt="gray" /></a></div><div class="third"><a href="http://example.org?tshirt=yellow" id="yellow"><img src="img/t-yellow.png" id="yellow" alt="yellow" /></a></div><div class="third"><a href="http://example.org?tshirt=green"><img src="img/t-green.png" id="green" alt="green" /></a></div></div>';
-$('#selector').append(vote);
+  // Variables to track votes for each t-shirt color
+  var voteCounts = {
+    gray: 0,
+    yellow: 0,
+    green: 0
+  };
 
-// This adds ratings to the side bar
-$('#selector a').on('click', function(e) {
-  e.preventDefault();
-  var queryString = 'vote=' + $(e.target).attr('id');
-  $.get('/php/votes.php', queryString, function(data) {
-    $('#selector').html(data);
+  // Add click event listeners to t-shirt images
+  $('#vote a').on('click', function(e) {
+    e.preventDefault();  // Prevent the default action of the anchor tag
+
+    var selectedColor = $(this).data('color');  // Get the color from data attribute
+    voteCounts[selectedColor]++;  // Increment the vote count for the selected color
+
+    // Update the vote count display
+    $('#' + selectedColor + '-count').text(voteCounts[selectedColor]);
+
+    // Optional: If you still want to send the vote to the server, you can use this
+    var queryString = 'vote=' + selectedColor;
+    $.get('/php/votes.php', queryString, function(data) {
+      // Handle the server response if needed
+      console.log(data);
+    });
   });
 });
