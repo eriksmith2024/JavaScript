277d039eeaa4b70ff072a8f720ea9dd83b81ce72
eriksmith2024 <eriksmith.career@hotmail.com>
2024-09-02 09:14:44 +0100

    update to code

---
+++ chapter11/js/slider.js
@@ -0,0 +1,5 @@
+$('.slider').each(function() {                                  // for every slider
+    var $this = $(this);                                        // get the current slider
+    var $group = this.find('.slide-group');                      // get the slide group (container)
+    var $slides = $this.find('.slide');
+})
