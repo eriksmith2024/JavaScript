25d59ed9138985d39caab16b24f7637cd4829727
eriksmith2024 <eriksmith.career@hotmail.com>
2024-09-03 12:45:31 +0100

    bug fix

--- chapter13/js/show-option.js
+++ chapter13/js/show-option.js
@@ -1,19 +1,20 @@
-(function(){
-    var form, options, other, otherText, hide;      // declare cariables
-    form = document.getElementById('how-heard');        // get the form
-    options = form.elements.heard;        // get the radio button
-    other = document.getElementById('other');      // other radio button
-    otherText = document.getElementById('other-Text');  // other text
-    otherText.className = 'hide';               // hide other text input
-
-    for (var i = [0]; i <options.length; i++) {     // loop through radio buttons
-        addEvent(options[i], 'click', radioChanged);       //Add event listener
+(function() {
+    var form, options, other, otherText, hide;           // Declare variables
+    form      = document.getElementById('how-heard');    // Get the form
+    options   = form.elements.heard;                     // Get the radio buttons
+    other     = document.getElementById('other');        // Other radio button
+    otherText = document.getElementById('other-text');   // Other text input
+    otherText.className = 'hide';                        // Hide other text input
+  
+    for (var i = [0]; i < options.length; i++) {         // Loop through radios
+      addEvent(options[i], 'click', radioChanged);       // Add event listener
     }
+  
     function radioChanged() {
-        hide = other.checked ? '' : 'hide';     // is other checked
-        otherText.className = hide;     // text input visibility
-        if (hide) {                     // if text input is hidden
-            otherText.value = '';           // empty its contents 
-        }
+      hide = other.checked ? '' : 'hide';                // Is other checked?
+      otherText.className = hide;                        // Text input visibility
+      if (hide) {                                        // If text input hidden
+        otherText.value = '';                            // Empty its contents
+      }
     }
-}());
+  }());
